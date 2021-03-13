93e3303a1580cc53892e2b8387f7f2b736eb765b
c20391893 <72614971+c20391893@users.noreply.github.com>
2021-03-13 15:10:18 +0000

    g

--- Single game/Assets/Scripts/PlayerMovement.cs
+++ Single game/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,25 +6,36 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     [SerializeField] private float moveSpeed, maxSpeed, jumpForce;
     [SerializeField] private Collider2D groundCheck;
     [SerializeField] private LayerMask groundLayers;
+    [SerializeField] private bool cancelJumpEnabled;
 
     private float moveDir;
-    private bool canJump;
     private Rigidbody2D myRB;
-
+    private bool canJump;
+    private SpriteRenderer mySprite;
 
     private void Start()
     {
         myRB = GetComponent<Rigidbody2D>();
+        mySprite = GetComponentInChildren<SpriteRenderer>();
     }
 
     private void FixedUpdate()
     {
+        if (moveDir > 0)
+        {
+            mySprite.flipX = false;
+        }
+
+        if (moveDir < 0)
+        {
+            mySprite.flipX = true;
+        }
         var moveAxis = Vector3.right * moveDir;
-        if (-maxSpeed < myRB.velocity.x && myRB.velocity.x < maxSpeed)
+
+        if (Mathf.Abs(myRB.velocity.x) < maxSpeed)
         {
             myRB.AddForce(moveAxis * moveSpeed, ForceMode2D.Force);
         }
@@ -36,20 +48,33 @@ public class PlayerMovement : MonoBehaviour
         {
             canJump = false;
         }
+
     }
+
     public void Move(InputAction.CallbackContext context)
     {
-        print(context.ReadValue<float>());
         moveDir = context.ReadValue<float>();
     }
+
+    public void Move(float moveAmt)
+    {
+        moveDir = moveAmt;
+    }
+
     public void Jump(InputAction.CallbackContext context)
     {
         if (canJump)
         {
-            print(context.ReadValue<bool>());
-            myRB.AddForce( Vector3.up * jumpForce, ForceMode2D.Impulse);
-            canJump = false;
+            if (context.started)
+            {
+                myRB.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+                canJump = false;
+            }
+        }
+
+        if (context.canceled && cancelJumpEnabled)
+        {
+            myRB.velocity = new Vector2(myRB.velocity.x, 0f);
         }
     }
-
 }
