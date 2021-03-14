7b559b8656f3d437d059333b008d7f0954aa1abc
c20391893 <72614971+c20391893@users.noreply.github.com>
2021-03-14 12:51:20 +0000

    hjk

--- Single game/Assets/Scripts/PlayerCombat.cs
+++ Single game/Assets/Scripts/PlayerCombat.cs
@@ -7,7 +7,14 @@ public class PlayerCombat : MonoBehaviour
 {
     [SerializeField] private Collider2D playerCheck;
     [SerializeField] private LayerMask playerLayers;
-    
+
+    private void Update()
+    {
+        if (playerCheck.IsTouchingLayers(playerLayers))
+        {
+            Destroy(gameObject);
+        }
+    }
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Player")
